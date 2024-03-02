e6901298d709a399c9ff84b09e3d5c25d256f211
000626574705 <shreetikagmm123@gmail.com>
2024-03-03 02:32:52 +0530

    sessionid

--- index.js
+++ index.js
@@ -1,20 +1,39 @@
 const express=require("express");
+const path=require('path')
 const { connectToMongoDB } = require("./connect");
+
 const URL =require('./models/url');
 const urlRoute= require('./routes/url');
+const staticRouter = require('./routes/staticRouter');
+const {request} =require("http");
+const userRouter=require('./routes/user');
 
 
 
 const app=express();
+app.set("view engine","ejs");  //we tell our appication that which engine we are going to use
+app.set("views",path.resolve("./views")); //we mention path where all ejs files 
+
+
 const PORT=8001;
 connectToMongoDB("mongodb://0.0.0.0:27017/short-url")
 .then(()=> console.log('Mongodb connected'));
 
 app.use(express.json());
+app.use(express.urlencoded({extended:false}));
+app.get("/test",async(req,res) =>{
+    const allUrls=await URL.find({});
+    return res.render("home",{
+        urls:allUrls,
+    });
+});
 
 app.use("/url",urlRoute);
+app.use("/user",userRouter);
+app.use("/",staticRouter);
 
-app.get('/:shortId',async(req,res) =>{
+
+app.get('/url/:shortId',async(req,res) =>{
     const shortId=req.params.shortId;
     const entry =await URL.findOneAndUpdate({
         shortId,
