222ea5590b64d1b888efd8006780f0a139c1e3a6
000626574705 <shreetikagmm123@gmail.com>
2024-03-05 14:56:44 +0530

    jwt token

--- service/auth.js
+++ service/auth.js
@@ -1,11 +1,29 @@
-const sessionIdToUserMap = new Map();
+const jwt = require("jsonwebtoken");
+const secret="Shha@1617";
 
-function setUser(id,user){
-    sessionIdToUserMap.set(id,user);
+
+//this function make tokens for user
+function setUser(user){
+    
+    return jwt.sign(
+        {
+        _id:user._id,
+        email:user.email,
+
+    },
+    secret);
 }
 
-function getUser(id){
-    return sessionIdToUserMap.get(id);
+//
+function getUser(token){
+   // return sessionIdToUserMap.get(id);
+   if(!token) return null;
+   try{
+    return jwt.verify(token,secret);
+   }catch(error){
+    return null;
+   }
+     
 }
 module.exports ={
     setUser,
